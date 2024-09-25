bd044deb97ab178795ee4295bd2387440ed90e70
Agus Cano <canoagustin247@gmail.com>
2024-09-25 20:50:54 -0300

    Mensaje de commit inicial

---
+++ script.js
@@ -0,0 +1,17 @@
+function clearDisplay() {
+    document.getElementById('display').value = '';
+}
+
+function appendToDisplay(value) {
+    document.getElementById('display').value += value;
+}
+
+function calculate() {
+    let display = document.getElementById('display').value;
+    try {
+        document.getElementById('display').value = eval(display);
+    } catch (error) {
+        document.getElementById('display').value = 'error';
+    }
+}
+
